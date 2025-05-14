605c7b22bc83f1e051f621440ebd6ce4734f92c7
kochamii-312 <kaoru.yoshida@keio.jp>
2025-05-14 23:19:09 +0900

    データファイルのパスを相対パスに変更し、データ読み込みの安定性を向上

--- day5/演習2/main.py
+++ day5/演習2/main.py
@@ -24,7 +24,7 @@ class DataLoader:
             return pd.read_csv(path)
         else:
             # ローカルのファイル
-            local_path = "data/Titanic.csv"
+            local_path = "../data/Titanic.csv"
             if os.path.exists(local_path):
                 return pd.read_csv(local_path)
 
