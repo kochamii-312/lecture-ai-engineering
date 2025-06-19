01de98c29d46d7df694b69940b4bd1e5d0b7becb
kochamii-312 <169051415+kochamii-312@users.noreply.github.com>
2025-06-20 07:22:31 +0900

    dataパスを修正

--- feedback_analyzer/data.py
+++ feedback_analyzer/data.py
@@ -4,10 +4,10 @@ import pandas as pd
 excel_file = 'data/Day1_アンケート_.xlsx'
 
 # 読み込むシート名（通常は1つ目のシート）
-df = pd.read_excel(excel_file, sheet_name=0)
+train_df = pd.read_excel(excel_file, sheet_name=0)
 
 # 保存先のCSVファイルのパス
 csv_file = 'Day1_アンケート.csv'
 
 # CSVとして保存（インデックスは不要な場合はFalseに）
-df.to_csv(csv_file, index=False)
+train_df.to_csv(csv_file, index=False)
