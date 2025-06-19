b70126dba7663963949fce537bd3f3834bcbb0d4
kochamii-312 <169051415+kochamii-312@users.noreply.github.com>
2025-06-20 07:01:29 +0900

    dataを追加

--- feedback_analyzer/data.py
+++ feedback_analyzer/data.py
@@ -1,3 +1,13 @@
 import pandas as pd
 
-train_df = pd.read_csv('.csv')
+# 変換元のExcelファイルのパス
+excel_file = 'data/Day1_アンケート.xlsx'
+
+# 読み込むシート名（通常は1つ目のシート）
+df = pd.read_excel(excel_file, sheet_name=0)
+
+# 保存先のCSVファイルのパス
+csv_file = 'csvsDay1_アンケート.csv'
+
+# CSVとして保存（インデックスは不要な場合はFalseに）
+df.to_csv(csv_file, index=False)
